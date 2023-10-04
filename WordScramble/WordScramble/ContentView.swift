c2c69433b58418166a5865dee20cf514c6941fb9
shawn120 <931908356@qq.com>
2023-10-03 21:02:52 -0700

    add day 30

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,34 +8,114 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"]
-
-        var body: some View {
-            List(people, id: \.self) {
-                Text("Static Row")
-                Text($0)
-            }
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    var body: some View {
+        NavigationView {
             List {
-                Text("Static Row")
-
-                ForEach(people, id: \.self) {
-                    Text($0)
+                Section {
+                    TextField("Enter Your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
                 }
-                let input = "a"
-                let letters = input.components(separatedBy: " ")
-                let letter = letters.randomElement()
-                let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "error"
-                Text(trimmed)
                 
-                let word = "swift"
-                let checker = UITextChecker()
-                let range = NSRange(location: 0, length: word.utf16.count)
-                let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-                let allGood = misspelledRange.location == NSNotFound
-                Text(String(allGood))
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
             }
-               
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) {}
+            } message: {
+                Text(errorMessage)
+            }
         }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else {
+            return
+        }
+        guard isOriginal(word: answer) else {
+            wordError(title: "Answer is used already", message: "Try think about something else")
+            return
+        }
+        guard isPossible(word: answer) else {
+            wordError(title: "\"\(answer)\" is not valid", message: "You can only use the letter from \(rootWord)")
+            return
+        }
+        guard isReal(word: answer) else {
+            wordError(title: "\"\(answer)\" is not a word", message: "Check your spelling")
+            return
+        }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+    
+    func startGame() {
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // 3. Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+                // 4. Pick one random word, or use "silkworm" as a sensible default
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                // If we are here everything has worked, so we can exit
+                return
+            }
+        }
+        // If were are *here* then there was a problem – trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var rootCopy = rootWord
+        for letter in word {
+            if let pos = rootCopy.firstIndex(of: letter) {
+                rootCopy.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        // make an UITextChecker instance
+        let checker = UITextChecker()
+        // NSRange to scan the entire length of the string
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
 }
 
 #Preview {
